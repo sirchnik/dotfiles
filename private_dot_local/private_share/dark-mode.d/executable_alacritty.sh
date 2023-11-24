30fa7d303ac9049652164b93a571b1bbff98d0e1
Niklas Sirch <sirniklax@gmail.com>
2023-11-24 14:23:38 +0100

    add darkmode

---
+++ private_dot_local/private_share/dark-mode.d/executable_alacritty.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+sed -r -i "s;/themes/themes/.*.yaml;/themes/themes/base16_default_dark.yaml;" .config/alacritty/alacritty.yml
---
+++ private_dot_local/private_share/light-mode.d/executable_alacritty.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sed -r -i "s;/themes/themes/.*.yaml;/themes/themes/night_owlish_light.yaml;" .config/alacritty/alacritty.yml
