ce4dd3d2fb075137e9e416520a747079e83473cf
Niklas Sirch <niklas.sirch@xitaso.com>
2024-02-11 13:31:00 +0100

    update wezterm win

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+function get_os()
+    return package.config:sub(1,1) == "\\" and "win" or "unix"
+end
+
 function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
     return "Builtin Solarized Dark"
@@ -13,6 +17,13 @@ end
 config.font =  wezterm.font_with_fallback {'IntoneMono NF', 'Symbols Nerd Font', 'Noto Sans Symbols'}
 config.window_background_opacity = 0.9
 color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.window_decorations = "RESIZE"
+config.check_for_updates = false
+
+if get_os() == "win" then
+  config.check_for_updates = true
+  config.default_prog = { "nu.exe" }
+end
 
 return config
 
