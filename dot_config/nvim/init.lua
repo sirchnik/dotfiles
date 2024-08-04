486302f15f9e7d7c4284209230342afe62ec00cb
Niklas Sirch-Xitaso <niklas.sirch@xitaso.com>
2024-08-04 21:45:24 +0200

    nvim darkman windows

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
+function is_win()
+    return package.config:sub(1,1) == "\\"
+end
+
 vim.o.clipboard = 'unnamedplus'
 vim.wo.number = true
 vim.o.scrolloff = 10
@@ -10,95 +14,58 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-{
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-	      require("tokyonight").setup({
-          transparent = true,
+local plugins = {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                transparent = true,
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        setup = {
+            --matchup = {
+            --   enable = true,
+            --}
+        }
+    },
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
+        "andymass/vim-matchup",
+        event = "VeryLazy",
+        config = function()
+            vim.g.matchup_matchparen_enabled = 0
+        end
+    },
+}
+
+if not is_win then
+    table.insert(plugins, {
+        '4e554c4c/darkman.nvim',
+        build = 'go build -o bin/darkman.nvim',
+        dependencies = { "folke/tokyonight.nvim" },
+        config = function()
+            require 'darkman'.setup({
+                change_background = true,
+                send_user_event = false,
+                colorscheme = { dark = "tokyonight", light = "tokyonight-day" }
+            })
+        end,
     })
-  end
-},
---{
---   "folke/flash.nvim",
---   event = "VeryLazy",
---   --@type Flash.Config
---   opts = {
---           mode = {
---       	    search = {
---       		    enabled = false
---       	    }
---           }
---   },
---   -- stylua: ignore
---   keys = {
---       {
---       "S",
---       mode = {"n", "o", "x"},
---       function()
---           require("flash").jump()
---       end,
---       desc = "Flash"
---       }
---   }
---},
-{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    setup = {
-        --matchup = {
-         --   enable = true,
-        --}
-    }
-},
-{
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-},
-{
-    "andymass/vim-matchup",
-    event = "VeryLazy",
-    config = function()
-	vim.g.matchup_matchparen_enabled = 0
-    end
-},
---{
---	"gbprod/yanky.nvim",
---	opts = {
---		system_clipboard = {
---			sync_with_ring = false,
---		},
---	},
---	keys = {
---		{ mode = { "n", "x" }, "p", "<Plug>(YankyPutAfter)" },
---		{ mode = { "n", "x" }, "P", "<Plug>(YankyPutBefore)" },
---		{ mode = { "n", "x" }, "gp", "<Plug>(YankyGPutAfter)" },
---		{ mode = { "n", "x" }, "gP", "<Plug>(YankyGPutBefore)" },
---		{ mode = "n", "<c-n>", "<Plug>(YankyCycleForward)" },
---		{ mode = "n", "<c-p>", "<Plug>(YankyCycleBackward)" },
---	},
---},
-{
-  '4e554c4c/darkman.nvim',
-  build = 'go build -o bin/darkman.nvim',
-  dependencies = { "folke/tokyonight.nvim" },
-  config = function()
-    require 'darkman'.setup(
-	{
-	  change_background = true,
-	  send_user_event = false,
-	  colorscheme = { dark = "tokyonight", light = "tokyonight-day" }
-	})
-  end,
-},
-})
+end
 
-
+require("lazy").setup(plugins)
