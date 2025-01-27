006bb20d06aba2f9ce705afbbd204d741ec5178a
Niklas Sirch <sirniklax@gmail.com>
2025-01-27 23:52:41 +0100

    add output switcher

--- private_dot_local/bin/executable_change-audio-output.bash
+++ private_dot_local/bin/executable_change-audio-output.bash
@@ -1,9 +1,31 @@
 #!/bin/bash
-sink1=alsa_output.pci-0000_00_1f.3.analog-stereo
-sink2=alsa_output.usb-Logitech_Zone_Vibe_125_2342MH00AGR8-00.analog-stereo
 
+CONFIG_FILE="~/.config/sink-switcher.conf"
+
+get_sink_from_config() {
+  local sink_number=$1
+  # Use awk to extract the sink value for the given key
+  awk -F '=' -v key="$sink_number" '$1 == key {print $2}' "$CONFIG_FILE"
+}
+
+# Ensure the configuration file exists
+if [[ ! -f $CONFIG_FILE ]]; then
+  echo "Error: Configuration file '$CONFIG_FILE' not found."
+  exit 1
+fi
+
+# Read the sink from the configuration file based on input argument
 case $1 in
-  1) pactl set-default-sink $sink1 ;;
-  2) pactl set-default-sink $sink2 ;;
-  *) pactl set-default-sink $sink1 ;;
+  1) sink=$(get_sink_from_config "1") ;;
+  2) sink=$(get_sink_from_config "2") ;;
+  *) sink=$(get_sink_from_config "1") ;;
 esac
+
+# Check if the sink was found
+if [[ -z $sink ]]; then
+  echo "Error: Sink not found for the given input."
+  exit 1
+fi
+
+# Set the default sink
+pactl set-default-sink "$sink"
